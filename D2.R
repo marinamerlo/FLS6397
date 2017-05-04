4c6c0a1425c30488c4e055652359cbffda51d6a6
Marina Merlo <marina27merlo@gmail.com>
2017-05-03 21:10:11 -0300

    Create D2.R

---
+++ D2.R
@@ -0,0 +1,52 @@
+install.packages("tidyverse")
+library(rvest)
+library(XML)
+library(tidyverse)
+
+#raspando a lista de links
+links <- html("http://www.ipu.org/wmn-e/classif-arc.htm")%>% 
+  html_nodes("a") %>% 
+  html_attr("href")
+#selecionando apenas os links que contém as páginas que queremos raspar
+#links <- links[4:226]
+links <- links[4:15]
+links
+
+
+#loop pra juntar tudo
+baseurl <- "http://www.ipu.org"
+dados <- data.frame()
+for (link in links) {
+  url <- paste(baseurl, link, sep = "")
+  lista.tabelas <- readHTMLTable(url)
+  tabela <- as.data.frame(lista.tabelas[[1]],stringsAsFactors = FALSE) %>%
+            filter(complete.cases(.))
+    dados <- bind_rows(tabela) %>%
+      mutate(id = link) %>%
+      mutate(id = gsub("/wmn-e/arc/classif", "", id)) %>%
+      mutate(id = gsub(".htm", "", id)) %>%
+      separate(id, into = c("date", "year"), sep = 4) %>%
+      separate(date, into = c("day", "month"), sep = 2)
+}
+
+
+
+tabela$id <- link
+
+tabela <- tabela %>%
+    mutate(id = gsub("/wmn-e/arc/classif", "", id)) %>%
+    mutate(id = gsub(".htm", "", id)) %>%
+    separate(id, into = c("date", "year"), sep = 4) %>%
+    separate(date, into = c("day", "month"), sep = 2)
+    
+
+
+
+##transformando
+dates <- as.data.frame(links)
+dates$fulldate<- gsub("/wmn-e/arc/classif", "", dates$links)
+dates$fulldate<- gsub(".htm", "", dates$fulldate)
+dates <- dates %>%
+  mutate(fulldate2 = fulldate) %>%
+  separate(fulldate2, into = c("date", "year"), sep = 4) %>%
+  separate(date, into = c("day", "month"), sep = 2)
